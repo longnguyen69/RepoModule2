6df50353ca91bc80a953762a8e0ad182c97500dc
longnguyen69 <nguyenthanhlongtlck@gmail.com>
2020-04-07 05:42:08 +0700

    add all

--- ArrayOOP/creatTable.php
+++ ArrayOOP/creatTable.php
@@ -9,7 +9,40 @@
 </head>
 <body>
 <?php
-
+if (isset($_POST['creat'])){
+    $rows = $_POST['row'];
+    $cols = $_POST['column'];
+}
 ?>
+<?php
+if (isset($_POST['findMax'])){
+    $arrayMatrix = $_POST['value'];
+    function checkMax($arr)
+    {
+        $max = $arr[0];
+        for ($i = 0; $i < count($arr); $i++) {
+            if ($max < $arr[$i]) {
+                $max = $arr[$i];
+            }
+        }
+        return "Max value = " . $max;
+    }
+    echo checkMax($arrayMatrix);
+}
+?>
+<form method="post">
+    <table>
+        <?php for ($i = 0; $i < $rows; $i++): ?>
+            <tr>
+                <?php for ($j = 0; $j < $cols; $j++): ?>
+                    <td><input type="number" name="value[]"></td>
+                <?php endfor ?>
+            </tr>
+        <?php endfor ?>
+        <tr>
+            <td><input type="submit" name="findMax" value="Max"></td>
+        </tr>
+    </table>
+</form>
 </body>
 </html>
